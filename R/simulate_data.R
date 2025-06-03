86244efd272999866c5056a254b95866b6a7a531
Alex O. Holcombe <aoholcombe@gmail.com>
2025-06-04 07:31:50 +1000

    Small changes to simulate_data

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -4,7 +4,7 @@ library(brms)
 # Creating Simulated Data
 # In order to build and test our model in brms, we must first create a simulated data set that is similar to our actual experiment data. This allows us to confirm the brms model is working and successfully recovers the parameters we set before applying it to our real experimental data that has unknown parameter values. In the actual data, there will be many group-wise differences in location and scale parameters. The following simulated data only has explicit differences between the $\eta$ (location) of the two age groups (older vs younger).
 
-generate_conditions_both_labs<- function(numSubjects,trialsPerCondition,targetNumConds,speedsPerCond) {
+generate_conditions<- function(laboratories,numSubjects,trialsPerCondition,targetNumConds,speedsPerCond) {
   #e.g.:
   #numSubjects<- 50
   #trialsPerCondition<- 5
@@ -14,19 +14,19 @@ generate_conditions_both_labs<- function(numSubjects,trialsPerCondition,targetNu
   
   #Generate data frame for simulated data
   sim_conditions <- tidyr::expand_grid(
-    subj = seq(1, numSubjects), #subjects
-    rep = seq(1,trialsPerCondition), #replicates of each trial combination
+    subjWithinGroup = seq(1, numSubjects), #subjects
+    trialThisCond = seq(1,trialsPerCondition), #replicates of each trial combination
     num_targets = targetNumConds, #number of targets particpant tracks
     speed = speeds
   )
   
   #Assign half of subjects to each lab, and set gender and age for each subject
   sim_conditions <- sim_conditions %>%
-    group_by(subj) %>%
+    group_by(subjWithinGroup) %>%
     mutate(
       gender = sample(c("M","F"), 1, replace = TRUE),
       age_group = sample(c("younger", "older"), 1, replace = TRUE),
-      lab = sample(c("Roudaia", "Holcombe"), 1, replace = TRUE) 
+      lab = sample(laboratories, 1, replace = TRUE) 
     ) %>% ungroup()
   
   # Set number of objects per ring based on lab
@@ -42,18 +42,81 @@ generate_conditions_both_labs<- function(numSubjects,trialsPerCondition,targetNu
 }
 
 
-self_test<-FALSE
+self_test<-TRUE
 if (self_test) {
-  numSubjects<- 50
-  trialsPerCondition<- 5
+  laboratories<- c("Roudaia", "Holcombe")
+  subjPerGroup<- 50
+  trialsPerCond<- 8
   targetNumConds<- c(2,3)
   #Array of speeds (not very realistic because mostly controlled by a staircase in actual experiment)
   speeds<-seq(.02,1.7, length.out = 12) # trials at 12 different speeds between .02 and 1.8
   
-  trials <- generate_conditions_both_labs(numSubjects,trialsPerCondition,targetNumConds,speeds)
+  trials <- generate_conditions(laboratories,subjPerGroup,
+                                trialsPerCond,targetNumConds,speeds)
   
   #Print number of unique values of each column
-  trials |> summarise(across(everything(), ~ n_distinct(.))) |>
-            pivot_longer(everything())
+  numValsPerFactor<- trials |> summarise(across(everything(), ~ n_distinct(.))) |>
+    pivot_longer(everything())
+  print( numValsPerFactor )
+  
+  ##sanity-check that unbalanced if filter weirdly:
+  trialsDefinitelyUnbalanced<- trials |> filter( ! ( subjWithinGroup != "1" & num_targets != "2" & trialThisCond==1) ) 
+  
+  #If below has more than one row, it means there are multiple different counts for the combinations—
+  #so the combinations are not balanced.
+  #If it has exactly one row, then all combinations occur equally often (i.e., are balanced).
+  unbalanced<- trials %>% 
+    group_by(lab, gender, age_group, subjWithinGroup, num_targets, obj_per_ring) %>%
+    summarise(numGroups = n(), .groups = "drop") %>%
+    count(numGroups)
+  unbalanced <- nrow(unbalanced) > 1
+  if (unbalanced) {
+    print('unbalanced lab, gender, age_group, subjWithinGroup, num_targets, obj_per_ring = ',
+          as.logical(unbalanced))
+  }
+  
+  #Confirm that each condition has trials numbered 1 to trialsPerCond
+  groupedByCond<- trials |> group_by(lab,gender,age_group,subjWithinGroup,
+                                     num_targets,obj_per_ring,speed) 
+  groupedByCond<- groupedByCond |> summarise(n=n(),.groups="drop") |> 
+    select(n)  |> distinct() #Should be one row only
+  if ((nrow(groupedByCond) != 1)  |  (groupedByCond$n[1] != trialsPerCond)) {
+    print("The conditions somehow don't have trialsPerCond trials")
+  }
+  
+  #Create "trial" that numbers trials by subject rather than by subj*cond
+  #So far I don't have a trial number for the subject, I only have a trialThisCond column
+  #Calculate a trial number numbering the entirety of the trials the subject is given
+  #Assume the within-participant factors are obj_per_ring,num_targets, and speed
+  trials <- trials |> group_by(lab,trialThisCond,age_group,gender,subjWithinGroup) |> 
+    mutate(trial = row_number())
+  cat('Trials per participant should be ',2*length(targetNumConds)*length(speeds),
+      'and it is:',max(trials$trial))
+  
+  #Check that subject behaves like I expect.
+  data_one_subject_group <- trials |> 
+    filter(age_group=="younger",gender=="M",lab=="Holcombe")
+  trialsPerSubject<- length(targetNumConds) * 2 * length(speeds) * trialsPerCond
+  subjectsTrials<- table(data_one_subject_group$subjWithinGroup)
+  distinctTrialsPerSubject <- unique(subjectsTrials)
+  if (length(distinctTrialsPerSubject) > 1) {
+    print('Somehow different subjects have different numbers of trials.')
+  }
+  cat('Trials per participant should be ',trialsPerSubject,
+      'and it is:',as.numeric(subjectsTrials[1]))
+  
+  numSsPerGroup <- length( table(data_one_subject_group$subjWithinGroup) )
+  if (numSsPerGroup != subjPerGroup) {
+    print("Not sure num subjects worked right.")
+  }
+  
+  data_one_subject_cond <- trials |> 
+    filter(num_targets==2,age_group=="younger",gender=="M",lab=="Holcombe")
+  
+  numSsPerGroup <- length( table(data_one_subject_group$subjWithinGroup) )
+  if (numSsPerGroup != subjPerGroup) {
+    print("Not sure num subjects worked right.")
+  }
+  
 }
 
