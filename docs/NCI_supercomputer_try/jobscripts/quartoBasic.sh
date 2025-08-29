8fb0125469ec5cfdaac4fe1887a559053e7de72d
Holcombe Alex <ha3155@gadi-login-09.gadi.nci.org.au>
2025-08-29 13:29:06 +1000

    added supercomputer basic jobscript

---
+++ docs/NCI_supercomputer_try/jobscripts/quartoBasic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PBS -P ew49
+#PBS -q normal
+#PBS -l ncpus=4
+#PBS -l mem=16gb
+#PBS -l walltime=00:10:00
+
+echo "Current directory is $(pwd)"
+echo "Path is $PATH"
+
+module load R/4.5.0
+
+echo "Contents of current directory are: $(ls)"
+echo "End of directory listing."
+my_NCI_scripts_dir=~/brms_model_recovery_MOT/brms_psychometric_variableGuessRate_lapseRate/docs/NCI_supercomputer_try
+
+echo "ls of my_NCI_scripts_dir is $(ls "$my_NCI_scripts_dir")"
+
+~/bin/quarto -v
